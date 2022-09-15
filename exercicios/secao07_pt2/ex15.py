fd8abb6551c22b405a6a54e340202af51e3a0639
Edson Pimenta <edson.tibo@gmail.com>
2022-09-15 11:46:16 -0300

    Ex 15 realizado e criado requirements.txt

--- exercicios/secao07_pt2/ex15.py
+++ exercicios/secao07_pt2/ex15.py
@@ -9,7 +9,57 @@ aluno.
 
 
 def main() -> None:
-    pass
+    gabarito = []
+
+    cont = 0
+    while len(gabarito) < 10:
+        resposta = str(input(f"Digite a alternativa correta da questão {cont + 1}: "))
+
+        if (resposta.lower() == 'a') or (resposta.lower() == 'b') or (resposta.lower() == 'c') or (
+                resposta.lower() == 'd'):
+            gabarito.append(resposta)
+
+        else:
+            print("Alternativa inválida")
+            cont -= 1
+
+        cont += 1
+
+    print()
+
+    alunos = []
+    for i in range(5):
+
+        respostas = []
+        for j in range(10):
+
+            resposta = str(input(f"Digite a alternativa da questão {j + 1} do {i + 1} aluno: "))
+
+            if (resposta.lower() == 'a') or (resposta.lower() == 'b') or (resposta.lower() == 'c') or \
+                    (resposta.lower() == 'd'):
+
+                respostas.append(resposta)
+
+            else:
+
+                # Caso o aluno digite uma alternativa inválida ou deixe em branco, a questão estará errada
+                respostas.append('z')
+
+        print()
+        alunos.append(respostas)
+
+    resultado = {}
+    for i in range(5):
+
+        corretos = 0
+        for j in range(len(alunos[i])):
+            if alunos[i][j] == gabarito[j]:
+                corretos += 1
+
+        resultado[f'aluno {i + 1}'] = corretos
+
+    for aluno, resultado in resultado.items():
+        print(f"O {aluno} teve {resultado} acertos")
 
 
 if __name__ == '__main__':
