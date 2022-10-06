903b1297dad225770df699c1aac722722bd41395
Edson Pimenta <edson.tibo@gmail.com>
2022-10-06 12:25:30 -0300

    Criado arquivo para exercício 3 da seção 16

---
+++ exercicios/secao16/ex03.py
@@ -0,0 +1,28 @@
+"""
+3) Crie uma classe denominada Elevador para armazenar as informações de um
+elevador dentro de um prédio. A classe deve armazenar o andar atual (térreo = 0),
+total de andares no prédio, excluindo o térreo, capacidade do elevador, e
+quantas pessoas estão presentes nele.
+A classe deve também disponibilizar os seguintes métodos:
+    . Inicializa: que deve receber como parâmetros a capacidade do elevador e
+    o total de andares no prédio (os elevadores sempre começam no térreo vazio);
+    . Entra: para acrescentar uma pessoa no elevador (só deve acrescentar se ainda
+    houver espaço);
+    . Sai: para remover uma pessoa do elevador (só deve remover se houver alguém
+    dentro dele);
+    . Sobe: para subir um andar (não deve subir se já estiver no último andar);
+    . Desce: para descer um andar (não deve descer se já estiver no térreo);
+    . Encapsular métodos os atributs da classe (criar métodos set e get).
+"""
+
+
+class Elevator:
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
