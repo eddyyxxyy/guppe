ab6d606f329638d6562d5ef7ee0ef4521095cebb
Edson Pimenta <edson.tibo@gmail.com>
2022-10-17 14:24:36 -0300

    Realizada a aula sobre assertions

---
+++ aulas/secao20_testes_python/assertions.py
@@ -0,0 +1,51 @@
+"""
+Assertions (Declarações/Checagens/Questionamentos)
+
+Em Python, utilizamos a palavra reservada assert para
+realizar simples afirmações utilizadas nos testes.
+
+Utilizamos o assert em uma expressão que queremos
+checar se é válida ou não. Se a expressão for True,
+o assert retorna None e caso seja falsa, levanta um
+erro do tipo AssertionError.
+
+# OBS: Nós podemos especificar, opcionalmente, um
+segundo argumento ou mesmo uma mensagem de erro
+personalizada.
+
+# OBS: A palavra assert pode ser utilizada em qualquer
+função ou código nosso. Não precisa ser exclusivamente
+nos testes, mas não tem seu uso voltado para tal que
+não sejam os testes/debug.
+
+
+# ALERTA -> Cuidado ao utilizar 'assert'
+Se um programa Python for executado com o parâmetro
+-O, nenhum assertion será validado. Ou seja, todas
+as suas validações já eram.
+"""
+
+
+def soma_numeros_positivos(a, b):
+    assert a > 0 and b > 0, 'Ambos números precisam ser positivos'
+    return a + b
+
+
+ret = soma_numeros_positivos(2, 4)  # 6
+# ret = soma_numeros_positivos(-2, 4)  # AssertionError
+# print(ret)
+
+
+def comer_fast_food(fast_food):
+    assert fast_food in [
+        'pizza',
+        'sorvete',
+        'doces',
+        'batata frita',
+        'cachorro quente',
+    ], 'A comida precisa ser fast food.'
+    return f'Eu estou comendo {fast_food}'
+
+
+comida = input('Informe a comida: ')
+print(comer_fast_food(comida))
