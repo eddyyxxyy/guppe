e73eec7c2fdd83dc4b4cca1756a269c40b073131
Edson Pimenta <edson.tibo@gmail.com>
2022-10-18 20:16:12 -0300

    Adicionados novos métodos

--- aulas/secao20_testes_python/atividades/atividades.py
+++ aulas/secao20_testes_python/atividades/atividades.py
@@ -11,3 +11,10 @@ def dormir(num_horas):
         return f'Ptz! Dormi muito! Estou atrasado para o trabalho!'
     else:
         return f'Continuo cansado após dormir por {num_horas} horas... :('
+
+
+def eh_engracada(pessoa):
+    comediantes = ['Jim Carrey', 'Bozo']
+    if pessoa in comediantes:
+        return True
+    return False
