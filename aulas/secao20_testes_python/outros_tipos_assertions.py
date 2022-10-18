5f666d5112fbb3a62dc3f020e7b205c4ca7b7d39
Edson Pimenta <edson.tibo@gmail.com>
2022-10-18 20:14:47 -0300

    Realizada a aula sobre os tipos de assertions Unittest

---
+++ aulas/secao20_testes_python/outros_tipos_assertions.py
@@ -0,0 +1,21 @@
+"""
+Unittest - Outros tipos de Assertions
+
+Já utilizamos a assertEqual(a, b), que verifica
+se o valor a é igual a b.
+
+Conhecendo as Assertions:
+
+assertEqual(a, b)          |    a == b
+assertNotEqual(a, b)       |    a != b
+assertTrue(a)              |    bool(a) is True
+assertFalse(a)             |    bool(a) is False
+assertIs(a, b)             |    a is b
+assertIsNot(a, b)          |    a is not b
+assertIsNone(a)            |    a is None
+assertIsNotNone(a)         |    a is not None
+assertIn(a, b)             |    a in b
+assertNotIn(a, b)          |    a not in b
+assertIsInstance(a, b)     |    isinstance(a, b)
+assertIsNotInstance(a, b)  |    not isinstance(a, b)
+"""
