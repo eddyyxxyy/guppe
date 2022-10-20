6e44438a41ccb855f1dce1126324e4629c99ea05
Edson Pimenta <edson.tibo@gmail.com>
2022-10-20 12:43:31 -0300

    Anotações sobre tipagem em Python e outros langs

---
+++ aulas/secao22_checagem_de_tipos/tipagem_de_dados_dinamica_estatica.py
@@ -0,0 +1,37 @@
+"""
+Tipagem de Dados: Dinâmica x Estática
+
+As anotações de tipos (ou dicas de tipo), foram implementadas
+no Python a partir de sua versão 3.5 e são usadas para indicar
+os tipos de dados das variáveis e dos valores de entrada e
+saída de funções e métodos em um programa escrito em Python
+ou em outras linguagens de programação.
+
+Lembrando que em muitas linguagens de programação a declaração
+de tipos é obrigatória (como em linguagem C, que usa tipos
+estáticos), e se o tipo correto não for declarado, o programa
+apresentará erros, podendo até mesmo não compilar.
+
+Não é o caso com o Python, que é uma linguagem dinamicamente
+tipada.
+
+Desta forma:
+
+Tipo estático — Uma verificação de tipo é realizada em tempo
+de compilação ou interpretação e requer declarações de tipo
+de dados. O código é mais explícito.
+
+Tipo dinâmico — Verificações de tipos são realizadas em
+tempo de execução, e assim não requer declarações de
+tipo de dados.
+
+As anotações de tipo são dicas que tornam o código mais
+fácil de ler e entender, mas é preciso ter em mente que
+elas NÃO forçam os tipos nas variáveis e funções onde
+são usadas – são, na verdade, uma maneira adicional de
+documentar o código e assim auxiliar em sua depuração,
+caso ocorram erros ao executá-lo, pois é mais fácil
+detectar erros de tipo se o tipo estiver discriminado
+ao lado do elemento tipado – as variáveis e funções
+do script.
+"""
