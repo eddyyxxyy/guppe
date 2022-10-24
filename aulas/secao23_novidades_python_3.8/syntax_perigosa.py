0f085343f6d1eb06376a689f57586d6edbc761b2
Edson Pimenta <edson.tibo@gmail.com>
2022-10-24 12:57:21 -0300

    Anotações sobre Syntax Warnings

---
+++ aulas/secao23_novidades_python_3.8/syntax_perigosa.py
@@ -0,0 +1,15 @@
+"""
+Alertas sobre Sintaxes Perigosas
+
+É comum os desenvolvedores confundirem o uso
+do "is" e do "==".
+
+== -> Usado para checar valor;
+is -> Usado para checar se os objetos são os mesmos.
+
+Temos agora, à partir do Python 3.8, o SyntaxWarning.
+
+O SyntaxWarning é um aviso, não um erro, que informa
+ao desenvolvedor o que provavelmente ocasionou o erro
+ou resultado inesperado na execução do código.
+"""
